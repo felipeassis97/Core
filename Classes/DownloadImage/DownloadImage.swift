79f6cf3b9461ac60a08b48625261f18e1984dffe
felipeassis97 <felipeassis97@gmail.com>
2024-06-18 21:35:43 +1000

    feat: Use KingFisher for downmload images

--- Classes/DownloadImage/DownloadImage.swift
+++ Classes/DownloadImage/DownloadImage.swift
@@ -6,12 +6,45 @@
 //
 
 import Foundation
+import Kingfisher
 import UIKit
+import SwiftUI
 
-struct DownloadImage: IDownloadImage {
-    func download(from imageURL: String) async throws -> UIImage? {
-        
+//struct DownloadImage: IDownloadImage {
+//    func download(from imageURL: String) async throws -> UIImage? {
+//
+//        let imageCache = NSCache<NSString, UIImage>()
+//        guard let url = URL(string: imageURL) else {
+//            return UIImage(systemName: "exclamationmark.triangle.fill")
+//        }
+//
+//        //Check if exists in cache
+//        if let cachedImage = imageCache.object(forKey: imageURL as NSString) {
+//            return cachedImage
+//        }
+//
+//        do {
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//            guard let image =  UIImage(data: data) else {
+//                return UIImage(systemName: "exclamationmark.triangle.fill")
+//            }
+//            //Save in cache
+//            imageCache.setObject(image, forKey: imageURL as NSString)
+//            return image
+//        }
+//        catch {
+//            print("Error when download image: \(error)")
+//            return UIImage(systemName: "exclamationmark.triangle.fill")
+//        }
+//    }
+//}
+
+
+struct DownloadImageKingFisher: IDownloadImage {
+    func download(from imageURL: String) -> UIImage? {
         let imageCache = NSCache<NSString, UIImage>()
+        var uiImage: UIImage?
+        
         guard let url = URL(string: imageURL) else {
             return UIImage(systemName: "exclamationmark.triangle.fill")
         }
@@ -21,18 +54,29 @@ struct DownloadImage: IDownloadImage {
             return cachedImage
         }
         
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            guard let image =  UIImage(data: data) else {
-                return UIImage(systemName: "exclamationmark.triangle.fill")
+        let _ =  KFImage.url(url)
+            .placeholder({
+                Rectangle()
+                    .scaledToFill()
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .foregroundStyle(.gray.opacity(0.1))
+                    .padding(.horizontal, 24)
+            })
+            .onSuccess { result in
+                guard let data = result.image.pngData() else {
+                    return
+                }
+                
+                if let imageData = UIImage(data: data) {
+                    uiImage = imageData
+                    imageCache.setObject(imageData, forKey: imageURL as NSString)
+                }
             }
-            //Save in cache
-            imageCache.setObject(image, forKey: imageURL as NSString)
-            return image
-        }
-        catch {
-            print("Error when download image: \(error)")
-            return UIImage(systemName: "exclamationmark.triangle.fill")
+        
+        if(uiImage != nil) {
+            return uiImage
+        } else {
+            return UIImage(named: "exclamationmark.triangle.fill")
         }
     }
 }
